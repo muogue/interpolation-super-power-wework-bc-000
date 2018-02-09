c9908031d12a0f76c0fd76ecf14b7dd0130aed49
Vincent Huang <vincent.huang@wework.com>
2018-02-09 01:53:36 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,9 +1,10 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  output = ""
-  colors.each do |color|
-    output += "#{color[0].capitalize}: #{color}, "
-  end
-  output = output[0..-3]
-  puts output
+  # output = ""
+  # colors.each do |color|
+  #   output += "#{color[0].capitalize}: #{color}, "
+  # end
+  # output = output[0..-3]
+  # puts output
+  puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
 end
